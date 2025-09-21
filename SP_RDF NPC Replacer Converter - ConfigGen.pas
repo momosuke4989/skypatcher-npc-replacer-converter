0752bd2d2713f434d73d83f4a32b0d67a55a01a3
momosuke4989 <momosuke4989@gmail.com>
2025-09-21 16:13:17 +0900

    integrate ConfigGen files

--- SP_RDF NPC Replacer Converter - ConfigGen.pas
+++ SP_RDF NPC Replacer Converter - ConfigGen.pas
@@ -4,14 +4,14 @@ const
   USE_EDITOR_ID = false;
 
 var
-  // iniファイル出力用変数
+  // 設定ファイル出力用変数
   slExport: TStringList;
   targetFileName, replacerFileName: string;
   
   // イニシャル処理で設定・使用する変数
-  useFormID, disableAll: boolean;
+  useSkyPatcher, useFormID, disableAll, replaceVS, replaceSkin, forceEnableRace, forceEnableGender, forceEnableName, forceEnableVoiceType: boolean;
 
-function ShowCheckboxForm(const options: TStringList; out selected: TStringList): Boolean;
+function ShowCheckboxForm(const options: TStringList; out selected: TStringList; caption: string): Boolean;
 var
   form: TForm;
   checklist: TCheckListBox;
@@ -22,7 +22,7 @@ begin
 
   form := TForm.Create(nil);
   try
-    form.Caption := 'Select Options';
+    form.Caption := caption;
     form.Width := 350;
     form.Height := 300;
     form.Position := poScreenCenter;
@@ -35,8 +35,13 @@ begin
     // 選択肢を追加
     for i := 0 to options.Count - 1 do begin
       checklist.Items.Add(options[i]);
+      // skinオプションはデフォルトをオンに設定
+      if (options[i] = 'Replace Visual Style') or (options[i] = 'Replace Skin') then
+        checklist.Checked[i] := true;
     end;
     
+
+    //checklist.Checked[2] := True;
 
     btnOK := TButton.Create(form);
     btnOK.Parent := form;
@@ -145,30 +150,67 @@ begin
 
 end;
 
+function GenerateVisualStyleString(const targetID, replacerID: string; useSkyPatcher: boolean): string;
+begin
+  Result := '';
+    if useSkyPatcher then
+      Result := 'filterByNpcs=' + TargetID + ':copyVisualStyle=' + ReplacerID
+    else
+      Result := 'match=' + TargetID + ' swap=' + ReplacerID;
+end;
 
 function Initialize: integer;
 var
-  validInput : boolean;
+  validInput: boolean;
   opts, selected: TStringList;
+  checkBoxCaption: string;
   i: Integer;
 begin
   slExport            := TStringList.Create;
 
+  useSkyPatcher       := false;
   useFormID           := false;
   
-  disableAll      := false;
+  disableAll          := false;
+  useFormID           := false;
+  replaceVS           := false;
+  replaceSkin         := false;
+  
+  forceEnableRace         := false;
+  forceEnableGender       := false;
+  forceEnableName         := false;
+  forceEnableVoiceType    := false;
   
   opts                := TStringList.Create;
   selected            := TStringList.Create;
   
+  checkBoxCaption := '';
+  
   Result              := 0;
+  
+  if MessageDlg('Which do you want to generate config file for SkyPatcher or RDF?' + #13#10 + 'Yes: SkyPatcher  No: RDF', mtConfirmation, [mbYes, mbNo], 0) = mrYes then
+    useSkyPatcher := true;
+  
+  if useSkyPatcher then
+    checkBoxCaption := 'Choose SkyPatcher Option'
+  else
+    checkBoxCaption := 'Choose RDF Option';
 
   // 各オプションの設定
   try
     opts.Add('Use Form ID for config file output');
     opts.Add('Disable the config file by default');
+    
+    if useSkyPatcher then begin
+      opts.Add('Replace Visual Style');
+      opts.Add('Replace Skin');
+      opts.Add('Force Replace Race');
+      opts.Add('Force Replace Gender');
+      opts.Add('Force Replace Name');
+      opts.Add('Force Replace VoiceType');
+    end;
 
-    if ShowCheckboxForm(opts, selected) then
+    if ShowCheckboxForm(opts, selected, checkBoxCaption) then
     begin
       AddMessage('You selected:');
       for i := 0 to selected.Count - 1 do
@@ -189,28 +231,62 @@ begin
     if selected[1] = 'True' then
       disableAll := true;
       
+    // SkyPatcher利用時のオプション設定
+    if useSkyPatcher and (selected.Count > 7) then begin
+      // 見た目を変更するか
+      if selected[2] = 'True' then
+        replaceVS := true;
+        
+      // 肌を変更するか
+      if selected[3] = 'True' then
+        replaceSkin := true;
+        
+      // 種族を強制的に変更するか
+      if selected[4] = 'True' then
+        forceEnableRace := true;
+        
+      // 性別を強制的に変更するか
+      if selected[5] = 'True' then
+        forceEnableGender := true;
+        
+      // 名前を強制的に変更するか
+      if selected[6] = 'True' then
+        forceEnableName := true;
+        
+      // 音声タイプを強制的に変更するか
+      if selected[7] = 'True' then
+        forceEnableVoiceType := true;
+        
+    end;
   finally
     opts.Free;
     selected.Free;
   end;
-  
+
 end;
 
 function Process(e: IInterface): integer;
 
 var
-  replacerFlags, templateFlags, targetFlags: IInterface;
-  targetRecord: IwbMainRecord;
+  replacerFlags, templateFlags, replacerRaceElement, replacerVoiceTypeElement, targetFlags, targetRaceElement, targetVoiceTypeElement: IInterface;
+  targetRecord, replacerRaceRecord, replacerVoiceTypeRecord, targetRaceRecord, targetVoiceTypeRecord: IwbMainRecord;
+  replacerName, targetName: string;
   replacerFormID, underscorePos: Cardinal;
   originalTargetID, recordSignature, targetFormID, targetEditorID, replacerEditorID: string; // レコードID関連
-  commentOutCopyVS: string; //コメントアウト用変数
-  trimedTargetFormID, trimedReplacerFormID, slTargetID, slReplacerID, wnamID, slSkinID: string; // SkyPatcher iniファイルの記入用
-  useTraits: boolean;
+  coChar, commentOutCopyVS, commentOutSkin, commentOutRace, commentOutGender, commentOutName, commentOutVoiceType: string; //コメントアウト用変数
+  trimedTargetFormID, trimedReplacerFormID, slTargetID, slReplacerID, wnamID, slSkinID, slRace, slGender, slName, slVoiceType: string; // SkyPatcher iniファイルの記入用
+  useTraits, sameRace, sameGender, sameName, sameVoiceType: boolean;
 begin
   targetFormID    := '';
   targetEditorID  := '';
   
+  coChar               := '';
   commentOutCopyVS     := '';
+  commentOutSkin       := '';
+  commentOutRace       := '';
+  commentOutGender     := '';
+  commentOutName       := '';
+  commentOutVoiceType  := '';
   
   recordSignature := 'NPC_';
 
@@ -219,7 +295,7 @@ begin
     AddMessage(GetElementEditValues(e, 'EDID') + ' is not NPC record.');
     Exit;
   end;
-
+  
   // リプレイサーMod名を取得
   replacerFileName := GetFileName(GetFile(e));
   
@@ -252,6 +328,11 @@ begin
   
   // リプレイサーNPCのフラグ、種族、名前、音声タイプを取得
   replacerFlags := ElementByPath(e, 'ACBS - Configuration');
+  replacerRaceElement := ElementByPath(e, 'RNAM');
+  replacerRaceRecord := MasterOrSelf(LinksTo(replacerRaceElement));
+  replacerName := GetElementEditValues(e, 'FULL');
+  replacerVoiceTypeElement := ElementByPath(e, 'VTCK');
+  replacerVoiceTypeRecord := MasterOrSelf(LinksTo(replacerVoiceTypeElement));
   
   // レコードがuse traitsフラグを持っているか確認し、持っていた場合はスキップ
   templateFlags := ElementByPath(replacerFlags, 'Template Flags');
@@ -262,10 +343,69 @@ begin
     Exit;
   end;
   
+  // ターゲットNPCのフラグ、種族、名前、音声タイプを取得
+  targetFlags := ElementByPath(targetRecord, 'ACBS - Configuration');
+  targetRaceElement := ElementByPath(targetRecord, 'RNAM');
+  targetRaceRecord := MasterOrSelf(LinksTo(targetRaceElement));
+  targetName := GetElementEditValues(targetRecord, 'FULL');
+  targetVoiceTypeElement := ElementByPath(targetRecord, 'VTCK');
+  targetVoiceTypeRecord := MasterOrSelf(LinksTo(targetVoiceTypeElement));
+  
+  // フォロワーNPCとターゲットNPCの種族、性別、音声タイプを比較して、結果をフラグに反映する。
+  if GetElementNativeValues(replacerRaceRecord, 'Record Header\FormID') = GetElementNativeValues(targetRaceRecord, 'Record Header\FormID') then
+    sameRace := true
+  else
+    sameRace := false;
+    
+  if GetElementEditValues(targetFlags, 'Flags\Female') = GetElementEditValues(replacerFlags, 'Flags\Female') then
+    sameGender := true
+  else
+    sameGender := false;
+    
+  if replacerName = targetName then
+    sameName := true
+  else
+    sameName := false;
+  
+  if GetElementNativeValues(replacerVoiceTypeRecord, 'Record Header\FormID') = GetElementNativeValues(targetVoiceTypeRecord, 'Record Header\FormID') then
+    sameVoiceType := true
+  else
+    sameVoiceType := false;
+  
+  // SkyPatcherかRDFの利用に応じてコメントアウト文字を切り替え
+  if useSkyPatcher then
+    coChar := ';'
+  else
+    coChar := '#';
+  
   // オプションの選択に応じて、設定行をコメントアウトする
-  if disableAll then
-    commentOutCopyVS    := '#';
-
+  if disableAll then begin
+    commentOutCopyVS    := coChar;
+    commentOutSkin      := coChar;
+    commentOutRace      := coChar;
+    commentOutGender    := coChar;
+    commentOutName      := coChar;
+    commentOutVoiceType := coChar;
+  end
+  else begin
+    if useSkyPatcher and not replaceVS then
+      commentOutCopyVS := coChar;
+      
+    if not replaceSkin then
+      commentOutSkin := coChar;
+    
+    if not forceEnableRace and sameRace then
+      commentOutRace := coChar;
+    
+    if not forceEnableGender and sameGender then
+      commentOutGender := coChar;
+    
+    if not forceEnableName and sameName then
+      commentOutName := coChar;
+    
+    if not forceEnableVoiceType and sameVoiceType then
+      commentOutVoiceType := coChar;
+  end;
   
   // 出力ファイル用の配列操作
   if useFormID then begin
@@ -279,12 +419,16 @@ begin
     
     trimedReplacerFormID := IntToHex(replacerFormID and  $FFFFFF, 1);
     
-    slTargetID := trimedTargetFormID + '~' + targetFileName;
-    slReplacerID := trimedReplacerFormID + '~' + replacerFileName;
+    slTargetID := targetFileName + '|' + trimedTargetFormID;
+    slReplacerID := replacerFileName + '|' + trimedReplacerFormID;
+    slRace  := GetFileName(replacerRaceRecord) + '|' + IntToHex(FormID(replacerRaceRecord) and  $FFFFFF, 1);
+    slVoiceType := GetFileName(replacerVoiceTypeRecord) + '|' + IntToHex(FormID(replacerVoiceTypeRecord) and  $FFFFFF, 1);
   end
   else begin
     slTargetID := targetEditorID;
     slReplacerID := replacerEditorID;
+    slRace  := EditorID(replacerRaceRecord);
+    slVoiceType := EditorID(replacerVoiceTypeRecord);
   end;
   
   // NPCレコードのWNAMフィールドが設定されていたらWNAMのスキンを反映。
@@ -292,12 +436,32 @@ begin
   wnamID := IntToHex(GetElementNativeValues(e, 'WNAM') and  $FFFFFF, 1);
   //  AddMessage('wnamID is:' + wnamID);
   if wnamID = '0' then
-    slSkinID := slReplacerID
+    slSkinID := 'null'
   else
-    slSkinID := wnamID + '~' + replacerFileName;
+    slSkinID := replacerFileName + '|' + wnamID;
+    
+  // 性別フラグを反映する文字列を入力
+  if GetElementEditValues(replacerFlags, 'Flags\Female') = 1 then
+    slGender := ':setFlags=female'
+  else
+    slGender := ':removeFlags=female';
   
-  slExport.Add('#' + GetElementEditValues(targetRecord, 'FULL'));
-  slExport.Add(commentOutCopyVS + 'match=' + slTargetID + ' swap=' + slReplacerID);
+  // 名前を入力
+  slName := replacerName;
+  
+  
+  slExport.Add(coChar + GetElementEditValues(targetRecord, 'FULL'));
+  //slExport.Add(commentOutCopyVS + 'filterByNpcs=' + slTargetID + ':copyVisualStyle=' + slReplacerID);
+  slExport.Add(commentOutCopyVS + GenerateVisualStyleString(slTargetID, slReplacerID, useSkyPatcher));
+  
+  // SkyPatcher利用時のみ出力する
+  if useSkyPatcher then begin
+    slExport.Add(commentOutSkin + 'filterByNpcs=' + slTargetID + ':skin=' + slSkinID);
+    slExport.Add(commentOutRace + 'filterByNpcs=' + slTargetID + ':race=' + slRace);
+    slExport.Add(commentOutGender + 'filterByNpcs=' + slTargetID + slGender);
+    slExport.Add(commentOutName + 'filterByNpcs=' + slTargetID + ':fullName=~' + slName + '~');
+    slExport.Add(commentOutVoiceType + 'filterByNpcs=' + slTargetID + ':voiceType=' + slVoiceType);
+  end;
   
   slExport.Add(#13#10);
 
@@ -306,21 +470,33 @@ end;
 function Finalize: integer;
 var
   dlgSave: TSaveDialog;
-  ExportFileName, saveDir: string;
+  ExportFileName, saveDir, filterString, fileExtension: string;
 begin
   if slExport.Count <> 0 then 
   begin
-  // SkyPatcher iniファイルの出力処理
-  saveDir := DataPath + 'SP_RDF NPC Replacer Converter\SKSE\Plugins\RaceSwap\';
+  
+  // SkyPatcherかRDFの利用に応じて出力先、拡張子を変更
+  if useSkyPatcher then begin
+    saveDir := DataPath + 'SP_RDF NPC Replacer Converter\SKSE\Plugins\SkyPatcher\npc\SP_RDF NPC Replacer Converter\';
+    filterString := 'Ini (*.ini)|*.ini';
+    fileExtension := '.ini';
+  end
+  else begin
+    saveDir := DataPath + 'SP_RDF NPC Replacer Converter\SKSE\Plugins\RaceSwap\';
+    filterString := 'Txt (*.txt)|*.txt';
+    fileExtension :=  '.txt';
+  end;
+  
+  // 設定ファイルの出力処理
   if not DirectoryExists(saveDir) then
     ForceDirectories(saveDir);
 
   dlgSave := TSaveDialog.Create(nil);
     try
       dlgSave.Options := dlgSave.Options + [ofOverwritePrompt];
-      dlgSave.Filter := 'Txt (*.txt)|*.txt';
+      dlgSave.Filter := filterString;
       dlgSave.InitialDir := saveDir;
-      dlgSave.FileName := replacerFileName + '.txt';
+      dlgSave.FileName := replacerFileName + fileExtension;
   if dlgSave.Execute then 
     begin
       ExportFileName := dlgSave.FileName;
